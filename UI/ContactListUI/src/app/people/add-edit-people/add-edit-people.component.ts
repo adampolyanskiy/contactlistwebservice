c99d5d9693ffcfcb299c314d11d3a81f6712b01a
Adam <adampolyanskiy@gmail.com>
2021-09-10 15:39:53 +0700

    added add/edit person modal

--- UI/ContactListUI/src/app/people/add-edit-people/add-edit-people.component.ts
+++ UI/ContactListUI/src/app/people/add-edit-people/add-edit-people.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-people',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditPeopleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+
+  @Input() person:any;
+  PersonId:string="";
+  PersonFirstName:string="";
+  PersonMiddleName:string="";
+  PersonLastName:string="";
+  PersonPhoneNumber:string="";
+  PersonAdress:string="";
+
 
   ngOnInit(): void {
+    this.PersonId = this.person.PersonId;
+    this.PersonFirstName = this.person.PersonFirstName;
+    this.PersonMiddleName = this.person.PersonMiddleName;
+    this.PersonLastName = this.person.PersonLastName;
+    this.PersonPhoneNumber = this.person.PersonPhoneNumber;
+    this.PersonAdress = this.person.PersonAdress;
   }
 
-}
+  addPerson(){
+    var val = {PeronId:this.PersonId,
+      PersonFirstName:this.person.PersonFirstName,
+      PersonMiddleName:this.person.PersonMiddleName,
+      PersonLastName:this.person.PersonLastName,
+      PersonPhoneNumber:this.person.PersonPhoneNumber,
+      PersonAdress:this.person.PersonAdress
+      };
+
+      this.service.addPerson(val).subscribe(res=>{
+        alert(res.toString());
+      });
+  }
+
+  updatePerson(){
+    var val = {PersonId:this.PersonId,
+              PersonFirstName:this.person.PersonFirstName,
+              PersonMiddleName:this.person.PersonMiddleName,
+              PersonLastName:this.person.PersonLastName,
+              PersonPhoneNumber:this.person.PersonPhoneNumber,
+              PersonAdress:this.person.PersonAdress
+      }
+    this.service.updatePerson(val).subscribe(res=>{
+      alert(res.toString());
+    });
+  }
+
+
+};
--- UI/ContactListUI/src/app/people/show-people/show-people.component.ts
+++ UI/ContactListUI/src/app/people/show-people/show-people.component.ts
@@ -6,6 +6,7 @@ import { SharedService } from 'src/app/shared.service';
   templateUrl: './show-people.component.html',
   styleUrls: ['./show-people.component.css']
 })
+
 export class ShowPeopleComponent implements OnInit {
 
   constructor(private service:SharedService) { }
@@ -15,6 +16,36 @@ export class ShowPeopleComponent implements OnInit {
   }
 
   PeopleList:any=[];
+
+  ModalTitle:string="";
+  ActiveAddEditPeopleComp:boolean=false;
+  person:any;
+
+
+  addClick(){
+    this.person={
+      PersonId:0,
+      PersonFirstName:"",
+      PersonMiddleName:"",
+      PersonLastName:"",
+      PersonPhoneNumber:"",
+      PersonAdress:""
+    }
+    this.ModalTitle="Добавить новый контакт";
+    this.ActiveAddEditPeopleComp=true;
+  }
+
+  editClick(item:any){
+    this.person=item;
+    this.ModalTitle="Изменить контакт";
+    this.ActiveAddEditPeopleComp=true;
+  }
+
+  closeClick(){
+    this.ActiveAddEditPeopleComp=false;
+    this.refreshPeopleList();
+  }
+
 
   refreshPeopleList() {
     this.service.getPeopleList().subscribe(data => {
