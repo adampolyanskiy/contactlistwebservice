9826d7236f7ab07b1e625fdab941f5d469653064
Adam <adampolyanskiy@gmail.com>
2021-09-10 13:28:53 +0700

    added bootstrap

--- UI/ContactListUI/src/app/app-routing.module.ts
+++ UI/ContactListUI/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PeopleComponent } from './people/people.component';
+
+const routes: Routes = [
+  {path:'people', component:PeopleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
