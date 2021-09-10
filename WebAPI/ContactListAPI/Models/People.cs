30cce8dd4efe8cab5f6f479935dbfbde0943d237
Adam <adampolyanskiy@gmail.com>
2021-09-10 11:57:10 +0700

    added Peron model

---
+++ WebAPI/ContactListAPI/Models/People.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactListAPI.Models
+{
+    public class People
+    {
+        public int PersonId { get; set; }
+
+        public string PersonFirstName { get; set; }
+
+        public string PersonMiddleName { get; set;  }
+
+        public string PersonLastNam { get; set; }
+
+        public string PersonPhoneNumber { get; set; }
+
+        public string PersonAdress { get; set; }
+    }
+}
